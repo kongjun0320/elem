86c19edda03b12229703348339bf820642c5d9da
kongjun0320 <1741142595@qq.com>
2021-04-16 07:05:42 +0800

    头部组件开发完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
   <div id="app">
-    <v-header />
+    <v-header :seller="seller" />
   </div>
 </template>
 
 <script>
 import VHeader from './components/v-header/v-header.vue'
+import { getSeller } from './api'
 export default {
   name: 'App',
+  data() {
+    return {
+      seller: {}
+    }
+  },
   components: {
     VHeader
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const { seller, goods, ratings } = require('./data.json')
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -11,6 +13,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
     }
   }
 }
