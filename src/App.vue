1822ba9433a2def82f34178e865edc5b16af8731
kongjun0320 <1741142595@qq.com>
2021-04-20 22:07:31 +0800

    老数据重构完成

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,81 @@
 <template>
-  <div id="app">
-    <v-header :seller="seller" />
+  <div id="app" @touchmove.prevent>
+    <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
-import VHeader from './components/v-header/v-header.vue'
+import qs from 'query-string'
 import { getSeller } from './api'
+import VHeader from './components/v-header/v-header'
+import Goods from './components/goods/goods'
+import Ratings from './components/ratings/ratings'
+import Seller from './components/seller/seller'
+import Tab from './components/tab/tab'
+
 export default {
-  name: 'App',
   data() {
     return {
-      seller: {}
+      seller: {
+        id: qs.parse(location.search).id
+      }
     }
   },
-  components: {
-    VHeader
+  computed: {
+    tabs() {
+      return [
+        {
+          label: '商品',
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '评论',
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '商家',
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ]
+    }
   },
   created() {
     this._getSeller()
   },
   methods: {
     _getSeller() {
-      getSeller().then((seller) => {
-        this.seller = seller
+      getSeller({
+        id: this.seller.id
+      }).then((seller) => {
+        this.seller = Object.assign({}, this.seller, seller)
       })
     }
+  },
+  components: {
+    Tab,
+    VHeader
   }
 }
 </script>
 
-<style lang="stylus"></style>
+<style lang="stylus" scoped>
+#app
+  .tab-wrapper
+    position: fixed
+    top: 136px
+    left: 0
+    right: 0
+    bottom: 0
+</style>
